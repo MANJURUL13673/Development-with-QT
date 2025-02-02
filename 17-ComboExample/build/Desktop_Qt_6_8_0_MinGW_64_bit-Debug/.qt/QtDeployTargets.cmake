f4f57febaa1af42abb1f7a0fd206779a60fd5a9e
Manjurul Haque <manjurulhaque552@gmail.com>
2025-02-02 20:20:13 +0600

    different widgets learning

---
+++ 17-ComboExample/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_17-ComboExample_FILE E:/PERSONAL/Learning/QT DEVELOPMENT/17-ComboExample/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/17-ComboExample.exe)
+set(__QT_DEPLOY_TARGET_17-ComboExample_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_17-ComboExample_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
---
+++ 19-ListWidgetExample/widget.cpp
@@ -0,0 +1,22 @@
+#include "widget.h"
+#include <QListWidget>
+#include <QVBoxLayout>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    QVBoxLayout *vbox = new QVBoxLayout(this);
+
+    QListWidget *listWidget = new QListWidget();
+    listWidget->setFont(QFont("Times", 15));
+    listWidget->setStyleSheet("background-color:yellow");
+
+    listWidget->insertItem(0, "C++");
+    listWidget->insertItem(1, "Python");
+    listWidget->insertItem(2, "Java");
+    listWidget->insertItem(3, "C#");
+
+    vbox->addWidget(listWidget);
+}
+
+Widget::~Widget() {}
