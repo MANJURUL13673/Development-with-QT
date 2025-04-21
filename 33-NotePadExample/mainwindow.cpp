dfc26f6cce4edc85a6684c9794b7cc36c912d8d2
Manjurul Haque <manjurulhaque552@gmail.com>
2025-04-21 17:39:35 +0600

    Notepad: save and save as

--- 33-NotePadExample/mainwindow.cpp
+++ 33-NotePadExample/mainwindow.cpp
@@ -19,6 +19,7 @@ void MainWindow::on_actionNew_triggered()
 {
     currentFile.clear();
     ui->textEdit->setText(QString());
+    setWindowTitle(QString("Notepad Application"));
 }
 
 
@@ -37,6 +38,51 @@ void MainWindow::on_actionOpen_triggered()
     QTextStream in(&file);
     QString text = in.readAll();
     ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QString fileName;
+
+    if(currentFile.isEmpty())
+    {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        currentFile = fileName;
+    }
+    else
+    {
+        fileName = currentFile;
+    }
+
+    QFile file(fileName);
+    if(!file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Can not save file " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save As");
+    QFile file(fileName);
+    if(!file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Can not save as file " + file.errorString());
+        return;
+    }
+
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
     file.close();
 }
 
