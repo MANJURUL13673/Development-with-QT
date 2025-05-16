338a52c689bc7f9d5d42effbbb80cab3800454ce
Manjurul Haque <manjurulhaque552@gmail.com>
2025-05-16 17:25:36 +0600

    Database connection with QT

---
+++ 41-SqliteConnection/widget.cpp
@@ -0,0 +1,29 @@
+#include "widget.h"
+#include "./ui_widget.h"
+#include <QSqlDatabase>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    QSqlDatabase sqlitedb = QSqlDatabase::addDatabase("QSQLITE");
+    sqlitedb.setDatabaseName("E:/PERSONAL/Learning/DATABASE/mydb.sqlite");
+
+    if(!sqlitedb.open()) {
+        ui->label->setText("Database is not connected");
+    }
+    else {
+        ui->label->setText("Database is connected");
+    }
+}
+
