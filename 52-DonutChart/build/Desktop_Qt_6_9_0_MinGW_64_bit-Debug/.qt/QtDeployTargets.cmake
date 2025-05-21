b040544e3321cf05cd2ef41b4035f4b932c45817
Manjurul Haque <manjurulhaque552@gmail.com>
2025-05-21 16:32:03 +0600

    Donut chart

---
+++ 52-DonutChart/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_52-DonutChart_FILE E:/PERSONAL/Learning/QT DEVELOPMENT/52-DonutChart/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/52-DonutChart.exe)
+set(__QT_DEPLOY_TARGET_52-DonutChart_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_52-DonutChart_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
