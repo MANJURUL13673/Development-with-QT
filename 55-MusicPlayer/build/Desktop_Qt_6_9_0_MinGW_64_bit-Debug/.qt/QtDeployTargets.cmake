5a3fe8e9ed938b4dda7a4f24505e4a0220eb095b
Manjurul Haque <manjurulhaque552@gmail.com>
2025-05-23 15:51:04 +0600

    Music player with QT

---
+++ 55-MusicPlayer/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_55-MusicPlayer_FILE E:/PERSONAL/Learning/QT DEVELOPMENT/55-MusicPlayer/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/55-MusicPlayer.exe)
+set(__QT_DEPLOY_TARGET_55-MusicPlayer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_55-MusicPlayer_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
---
+++ 55-MusicPlayer/mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMediaPlayer>
+#include <QAudioOutput>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void stateChanged(QMediaPlayer::PlaybackState state);
+    void positionChanged(qint64 position);
+
+private slots:
+    void on_playButton_clicked();
+
+    void on_pauseButton_clicked();
+
+    void on_stopButton_clicked();
+
+    void on_horizontalSliderPlay_sliderMoved(int position);
+
+    void on_horizontalSliderVolume_sliderMoved(int position);
+
+    void on_actionOpen_Music_triggered();
+
+    void on_volumeButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QMediaPlayer *player;
+    QAudioOutput *audioOutput;
+    bool muted;
+};
+#endif // MAINWINDOW_H
