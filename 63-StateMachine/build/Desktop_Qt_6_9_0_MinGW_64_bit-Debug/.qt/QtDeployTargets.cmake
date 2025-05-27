d4f46977a6fb05cf9a22fc627825663fcfc06254
Manjurul Haque <manjurulhaque552@gmail.com>
2025-05-27 11:46:12 +0600

    state machine to state change in qt

---
+++ 63-StateMachine/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_63-StateMachine_FILE E:/PERSONAL/Learning/QT DEVELOPMENT/63-StateMachine/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/63-StateMachine.exe)
+set(__QT_DEPLOY_TARGET_63-StateMachine_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_63-StateMachine_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6StateMachine.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
