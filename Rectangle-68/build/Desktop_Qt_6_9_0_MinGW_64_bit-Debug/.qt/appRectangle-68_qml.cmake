2f4ca7fe4454f9f592373aa03f04807da67d4690
Manjurul Haque <manjurulhaque552@gmail.com>
2025-05-29 14:20:32 +0600

    Rectangle drawing with Qt

---
+++ Rectangle-68/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appRectangle-68_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appRectangle-68")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/Rectangle-68")
+set(src_and_dest_list
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/Rectangle-68/Main.qml"
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/Rectangle-68/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Rectangle-68/Main.qml"
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/Rectangle-68/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appRectangle-68_qml.txt")
---
+++ Rectangle-68/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appRectangle-68_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appRectangle-68")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/Rectangle-68")
+set(src_and_dest_list
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/Rectangle-68/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appRectangle-68_res.txt")
