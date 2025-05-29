5e64a3affa068e0ec62410a0c8f394425bc12091
Manjurul Haque <manjurulhaque552@gmail.com>
2025-05-29 14:28:38 +0600

    Image element add in QML

---
+++ ImageElement-70/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appImageElement-70_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appImageElement-70")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70")
+set(src_and_dest_list
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70/Main.qml"
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ImageElement-70/Main.qml"
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appImageElement-70_qml.txt")
---
+++ ImageElement-70/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appImageElement-70_res.cmake
@@ -0,0 +1,9 @@
+
+set(target "appImageElement-70")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70")
+set(src_and_dest_list
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70/res.qrc"
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ImageElement-70/res.qrc"
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/ImageElement-70/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appImageElement-70_res.txt")
