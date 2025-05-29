c077efeb94b0dd6e59f473f9c5cd6b9f974a9397
Manjurul Haque <manjurulhaque552@gmail.com>
2025-05-29 17:31:33 +0600

    Anchor positioning

---
+++ Anchors-77/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appAnchors-77_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appAnchors-77")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/Anchors-77")
+set(src_and_dest_list
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/Anchors-77/Main.qml"
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/Anchors-77/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Anchors-77/Main.qml"
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/Anchors-77/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appAnchors-77_qml.txt")
---
+++ Anchors-77/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/appAnchors-77_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appAnchors-77")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/Anchors-77")
+set(src_and_dest_list
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/Anchors-77/build/Desktop_Qt_6_9_0_MinGW_64_bit-Release/.qt/appAnchors-77_res.txt")
