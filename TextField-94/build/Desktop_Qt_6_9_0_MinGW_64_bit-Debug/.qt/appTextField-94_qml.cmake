e44828794f2a271113b44cac073e223460274bbb
Manjurul Haque <manjurulhaque552@gmail.com>
2025-06-17 14:53:52 +0600

    TextField in QML

---
+++ TextField-94/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appTextField-94_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appTextField-94")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/TextField-94")
+set(src_and_dest_list
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/TextField-94/Main.qml"
+    "E:/PERSONAL/Learning/QT DEVELOPMENT/TextField-94/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/TextField-94/Main.qml"
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/TextField-94/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appTextField-94_qml.txt")
---
+++ TextField-94/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appTextField-94_res.cmake
@@ -0,0 +1,7 @@
+
+set(target "appTextField-94")
+set(working_dir "E:/PERSONAL/Learning/QT DEVELOPMENT/TextField-94")
+set(src_and_dest_list
+
+)
+set(timestamp_file "E:/PERSONAL/Learning/QT DEVELOPMENT/TextField-94/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appTextField-94_res.txt")
